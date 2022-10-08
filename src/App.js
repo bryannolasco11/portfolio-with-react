ac4427f1bc62dcb25378a14438c73beb20891149
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-10-07 22:09:02 -0500

    forgot to make a contact feature.  Already added the html for the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <About></About>
       </main>
     </div>
